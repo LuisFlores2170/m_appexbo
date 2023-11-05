06643e1d710cd8d5bea3cc9477abc8bbe93cc63c
LuisFlores2170 <hinojosafloresluisfernando@gmail.com>
2023-11-05 10:36:51 -0400

    update

---
+++ l10n_bo_lc/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- utf-8 -*-
+{
+    'name': 'Libro de compras',
+    'version': '1.0',
+    'depends': [
+        'base', 
+        'account'
+    ],
+    'author': 'APPEX BOLIVA',
+    'summary': 'Registro de datos de factura de proveedores para el libro de compras',
+    'data': [],
+    'installable': True,
+}
